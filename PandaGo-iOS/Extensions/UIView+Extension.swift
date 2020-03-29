c2bed14fa73a17b6b321272796b3c30eafab6b77
Suji Kim <rlatnwl9595@naver.com>
2020-03-28 18:50:02 -0700

    Add main page

---
+++ PandaGo-iOS/Extensions/UIView+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+Extension.swift
+//  PandaGo-iOS
+//
+//  Created by Suji Kim on 3/28/20.
+//  Copyright © 2020 sjkim. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
